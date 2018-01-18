27cfa1e273895fce8d7748ffa6f17936cf013408
Nathan Waddington <Nathan.Waddington@akqa.com>
2018-01-17 17:03:36 -0800

    linting

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -2,7 +2,6 @@ export default class Year {
   constructor(year) {
     this.year = year;
   }
-
   isLeap() {
     return this.year % 400 === 0 || (this.year % 4 === 0 && this.year % 100 !== 0);
   }
